88aa08d15ea373222d857b1a8890f367ac103e5a
unknown <nkarnwal07@gmail.com>
2021-02-11 22:37:03 +0530

    dew

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL= '/'
 LOGOUT_REDIRECT_URL= '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
