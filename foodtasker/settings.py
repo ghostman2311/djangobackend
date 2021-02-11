18be20130f059e094e77eb8ea567257d7458dd4f
unknown <nkarnwal07@gmail.com>
2021-02-11 22:48:17 +0530

    Ingire

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ MEDIA_URL = '/media/'
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
